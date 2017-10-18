fc370736aeb93587dff3342c716915869f7ea415
Renée Kooi <renee@kooi.me>
2017-10-18 12:50:57 +0200

    use babel-preset-env

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const defaultOptions = {
   modules: false,
+  targets: {
+    browsers: [
+      'last 2 Chrome versions',
+      'last 2 Firefox versions',
+      'last 2 Edge versions',
+    ],
+  },
 };
 
 module.exports = function buildPreset(context, options) {
   const finalOptions = Object.assign({}, defaultOptions, options);
   return {
     presets: [
-      [require('babel-preset-latest'), {
-        es2015: finalOptions,
-      }],
+      [require('babel-preset-env'), finalOptions]
     ],
     plugins: [
       require('babel-plugin-transform-class-properties'),
